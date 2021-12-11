b5042efc888d5263041244c4d9b1bf60f9735a3b
ben <x_z_gfree@163.com>
2021-12-11 23:53:13 +0800

    修改 修改生成模版

--- src/run.ts
+++ src/run.ts
@@ -2,40 +2,95 @@ import cac from 'cac';
 import inquirer from 'inquirer';
 import shell from 'shelljs';
 import { fs, path } from 'zx';
-import pkg from '../package.json';
+import templates from './templates';
+
+const curWorkDir = process.cwd();
+
+const { version } = JSON.parse(
+  fs.readFileSync(path.resolve(curWorkDir, './package.json'), 'utf-8'),
+);
+
+// console.log(version);
+
+const cacheDir = path.resolve(curWorkDir, './.coa_cache');
 
 const cli = cac('coa');
 
-cli.option('--url [repository]', 'custom git repository', {
-  default: '',
-});
+cli
+  .command('new [project]', 'creat new app')
+  .option('--url [repository]', 'custom git repository', {
+    default: '',
+  })
+  .action((project, options) => {
+    // console.log('project', project);
+    if (!project) {
+      console.log('请输入项目名，模版将复制到该目录下');
+      process.exit(1);
+    }
+
+    const projectDir = path.resolve(curWorkDir, `./${project}`);
+
+    // NOTE: 判断目录是否已存在
+    if (fs.existsSync(projectDir)) {
+      console.log(`项目：${project} 已存在，请修改项目名`);
+      process.exit(1);
+    }
+
+    const { url } = options;
+    if (url) {
+      create({ project, url, projectDir });
+    } else {
+      selectTemplate().then((url) => {
+        create({ project, url, projectDir });
+      });
+    }
+  });
 
 // Display help message when `-h` or `--help` appears
 cli.help();
 // Display version number when `-v` or `--version` appears
 // It's also used in help message
-cli.version(pkg.version);
+cli.version(version);
 
-// console.log(cli.parse());
+cli.parse();
 
-const { options } = cli.parse();
+function create({
+  project,
+  url,
+  projectDir,
+}: {
+  project: string;
+  url: string;
+  projectDir: string;
+}) {
+  if (!shell.which('git')) {
+    //在控制台输出内容
+    shell.echo('Sorry, this script requires git');
+    shell.exit(1);
+  }
 
-if (options.url) {
-  console.log('action');
-} else if ('h' in options || 'v' in options) {
-  process.exit(1);
-} else {
-  const templates = {
-    'solid-ts': {
-      url: 'https://github.com/xiamu14/pure-typescript-package.git',
-      desc: 'solid-js 的脚手架，使用 typescript 和 vite',
-    },
-    'command-line-ts': {
-      url: '',
-      desc: '命令行工具脚手架，使用 typescript 和 esm',
-    },
-  };
+  const projectCache = path.resolve(cacheDir, `./${project}`);
 
+  shell.exec(`git clone ${url} ${projectCache} -q`, function (code) {
+    if (code !== 0) {
+      // console.log('Program output:', stdout);
+      // console.log('Program stderr:', stderr);
+      process.exit(1);
+    } else {
+      // TODO: copy file
+      // console.log('Exit code:', code);
+      fs.copySync(projectCache, projectDir, {
+        filter: (src) => {
+          return !/\.git/.test(src);
+        },
+      });
+      fs.removeSync(cacheDir);
+      console.log('项目已生成');
+    }
+  });
+}
+
+async function selectTemplate() {
   const templateSelects = Object.keys(templates).map((key) => {
     const template = templates[key];
     return `[${key}] ${template.desc}`;
@@ -43,47 +98,29 @@ if (options.url) {
 
   // select
 
-  inquirer
-    .prompt([
-      {
-        type: 'list',
-        name: 'template',
-        message: '选择模版?',
-        choices: templateSelects,
-      },
-    ])
-    .then(async (answers) => {
-      console.info('Answer:', answers.template);
-      const keyMatch = answers.template.match(/^\[(\S+)\]/);
-      if (keyMatch) {
-        const key = keyMatch[1];
-        const template = templates[key];
-        console.info(template);
+  const answers = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'template',
+      message: '选择模版?',
+      choices: templateSelects,
+    },
+  ]);
 
-        await fs.mkdir(path.resolve(process.cwd(), './.cache'));
-        const { url } = template;
-        //检查控制台是否以运行`git `开头的命令
-        if (!shell.which('git')) {
-          //在控制台输出内容
-          shell.echo('Sorry, this script requires git');
-          shell.exit(1);
-        }
+  // console.info('Answer:', answers.template);
+  const keyMatch = answers.template.match(/^\[(\S+)\]/);
+  if (keyMatch) {
+    const key = keyMatch[1];
+    const template = templates[key];
 
-        //即同步运行外部工具
-        if (
-          shell.exec(
-            `git clone ${url} ${path.resolve(
-              process.cwd(),
-              `./.cache/${key}`,
-            )}`,
-          ).code !== 0
-        ) {
-          // loading
-          shell.exit(1);
-        }
-      } else {
-        console.error('代码运行错误，获取模版 key 错误');
-        process.exit(1);
-      }
-    });
+    console.info(template);
+
+    await fs.mkdir(cacheDir);
+    const { url } = template;
+    return url;
+    //检查控制台是否以运行`git `开头的命令
+  } else {
+    console.error('代码运行错误，获取模版 key 错误');
+    process.exit(1);
+  }
 }
---
+++ src/templates.ts
@@ -0,0 +1,12 @@
+const templates = {
+  'solid-ts': {
+    url: 'https://github.com/xiamu14/pure-typescript-package.git',
+    desc: 'solid-js 的脚手架，使用 typescript 和 vite',
+  },
+  'command-line-ts': {
+    url: '',
+    desc: '命令行工具脚手架，使用 typescript 和 esm',
+  },
+};
+
+export default templates;
